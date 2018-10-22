f10f133b0a1de0d5b5176935d9979e56faf29135
Aaditya Menon <aaditya@sparker0i.me>
2018-10-23 00:04:18 +0530

    Assignment 1 : Task 4

--- helloworld.c
+++ helloworld.c
@@ -1,5 +1,17 @@
 #include "mpich/mpi.h"
 #include "stdio.h"
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+
+float calc_avg(int *p , int s)
+{
+	float sum = 0;
+	for(int i = 0; i < s; i++){
+		sum += *(p + i);
+	}
+	return (float) (sum / s);
+}
 
 int main(int argc , char *argv)
 {
@@ -10,20 +22,36 @@ int main(int argc , char *argv)
 
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD , &world_rank);
-
-    int n;
+    float avg=0.0;
+	int sd;
+	int arr[world_size];
     if (world_rank == 0)
     {
-        scanf("%d",  &n);
-        //MPI_Send(&n , 1 , MPI_INT , 1 , 0 , MPI_COMM_WORLD);
-        MPI_Bcast(&n , 1 , MPI_INT , 0 , MPI_COMM_WORLD);
-    }
-    else
+		for (int i = 0; i < world_size; i++)
+        {
+			arr[i] = rand() % 100;
+			printf("%d " , arr[i]);
+		}
+		avg = calc_avg(arr , world_size);
+        printf("\nAverage is %f\n",avg);
+	}
+	MPI_Bcast(&avg , 1 , MPI_FLOAT , 0 , MPI_COMM_WORLD);
+	float rcv = pow(abs(arr[world_rank] - avg) , 2);
+	float *sub_avg = NULL;
+	float ans = 0.0;
+	if (world_rank == 0)
+		sub_avg = malloc(sizeof(float) * world_size);
+	MPI_Gather(&rcv , 1 , MPI_FLOAT , sub_avg , 1 , MPI_FLOAT , 0 , MPI_COMM_WORLD);
+	if (world_rank == 0)
     {
-        MPI_Bcast(&n , 1 , MPI_INT , 0 , MPI_COMM_WORLD);
-        printf("Process with Rank %d received number %d from Process 0\n" , world_rank , n);
-    }
-
+		for (int i = 0;i < world_size; i++) {
+			printf("%f\n" , *(sub_avg + i));
+			ans += *(sub_avg + i);
+		}
+		sd = (int) (ans/world_size);
+		sd = sqrt(sd);
+		printf("SD = %d\n", sd);
+	}
     MPI_Finalize();
     return 0;
 }
